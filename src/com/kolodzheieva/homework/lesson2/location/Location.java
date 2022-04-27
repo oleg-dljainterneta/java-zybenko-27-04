568d76ff679151539f169049fd93ce60d5c8f8aa
zybenko.oleg <zybenko.olge@gmail.com>
2022-04-27 17:05:53 +0300

    Add lesson 2

---
+++ src/com/kolodzheieva/homework/lesson2/location/Location.java
@@ -0,0 +1,17 @@
+package com.kolodzheieva.homework.lesson2.location;
+
+public class Location {
+
+    public static void main(String[] args) {
+
+        double latitude = 47.930850;
+        System.out.println("My latitude: " + latitude);
+
+        double longitude = 29.684725;
+        System.out.println("My longitude: " + longitude);
+
+    }
+
+}
+
+
