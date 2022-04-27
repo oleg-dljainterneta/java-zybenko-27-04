269f04e6e4d1c7b78ba00694e31003552bdfbc1e
zybenko.oleg <zybenko.olge@gmail.com>
2022-04-27 16:20:05 +0300

    Initial commit

---
+++ src/com/kolodzheieva/classwork/lesson2/Main.java
@@ -0,0 +1,11 @@
+package com.kolodzheieva.classwork.lesson2;
+
+public class Main {
+    //psvm
+    public static void main(String[] args) {
+
+        System.out.println("Hello in Idea");
+
+
+    }
+}
---
+++ src/com/kolodzheieva/classwork/lesson2/Variables.java
@@ -0,0 +1,43 @@
+package com.kolodzheieva.classwork.lesson2;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        byte myByte = 101;
+        System.out.println("this is byte: " + myByte);
+
+        short myShort = 30000;
+        System.out.println("this is short: " + myShort);
+
+        int myInt = 345_545_454;
+        System.out.println("this is int: " + myInt);
+
+        long myLong = 100_000_000_000L;
+        System.out.println("this is long: " + myLong);
+
+        float myFloat = 12.569F;
+        System.out.println("this is float: " + myFloat);
+
+        double myDouble = 56565656.546546546;
+        System.out.println("this is double: " + myDouble);
+
+        boolean isValid = false;
+        System.out.println("this is boolean: " + isValid);
+
+        char myChar ='*';
+        System.out.println("this is char1: " + myChar);
+
+        char myNumericChar = 2389;
+        System.out.println("this is char2: " + myNumericChar);
+
+        char ch3 = '\u0054';
+        System.out.println("this is char3: " + ch3);
+
+        String string = "test";
+        System.out.println("this is string: " + string);
+
+    }
+
+}
+
