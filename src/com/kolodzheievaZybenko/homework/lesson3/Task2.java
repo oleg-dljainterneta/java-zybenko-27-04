e57122b00fdaeb9332a2599c85d2e9da7b698f41
zybenko.oleg <zybenko.olge@gmail.com>
2022-05-04 17:53:48 +0300

    Add lesson 3

--- src/com/kolodzheievaZybenko/homework/lesson3/Task2.java
+++ src/com/kolodzheievaZybenko/homework/lesson3/Task2.java
@@ -4,9 +4,9 @@ public class Task2 {
 
   public static void main(String[] args) {
 
-    double a = 2, b = 5, c = 6, res;
-    res = Math.abs(a - b) / Math.pow(a + b, 3) - Math.sqrt(c);
-    System.out.printf("%.3f",res);
+    double a = 2, b = 5, c = 6, result;
+    result = Math.abs(a - b) / Math.pow(a + b, 3) - Math.sqrt(c);
+    System.out.println("Result; " + result);
 
 
   }
