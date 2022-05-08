3df834a5cb92c03c65d55782e3e5163d6c530940
zybenko.oleg <zybenko.olge@gmail.com>
2022-05-08 17:39:04 +0300

    correction lesson 5

--- src/com/kolodzheievaZybenko/homework/lesson5/luggage.java
+++ src/com/kolodzheievaZybenko/homework/lesson5/luggage.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Bag {
+public class luggage {
 
   public static void main(String[] args) throws IOException {
 
