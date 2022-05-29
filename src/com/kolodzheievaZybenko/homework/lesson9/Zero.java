80dbf9c8fbe8381bd97b662bac4992be933e59a6
Oleg <zybenko.oleg@gmail.com>
2022-05-29 11:45:10 +0300

    add lesson 9

--- src/com/kolodzheievaZybenko/homework/lesson9/Zero.java
+++ src/com/kolodzheievaZybenko/homework/lesson9/Zero.java
@@ -1,14 +1,10 @@
 package com.kolodzheievaZybenko.homework.lesson9;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Zero {
-    public static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         System.out.println("Enter the size of the array");
         int sizeArray = ArrayOptions.readNumber();
